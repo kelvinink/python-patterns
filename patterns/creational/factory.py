9e940c321f5c309048a1d7300882511168ed7811
kelvin <ziyoustep@gmail.com>
2019-06-06 19:56:54 +0800

    Update factory.py

--- patterns/creational/factory.py
+++ patterns/creational/factory.py
@@ -6,7 +6,7 @@ A Factory is an object for creating other objects.
 
 *What does this example do?
 The code shows a way to localize words in two languages: English and
-Greek. "get_localizer" is the factory function that constructs a
+Chinese. "get_localizer" is the factory function that constructs a
 localizer depending on the language chosen. The localizer object will
 be an instance from a different class according to the language
 localized. However, the main code does not have to worry about which
@@ -32,48 +32,35 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 
-class GreekLocalizer(object):
-    """A simple localizer a la gettext"""
-
-    def __init__(self):
-        self.translations = {"dog": "σκύλος", "cat": "γάτα"}
+class ChineseLocalizer(object):
+    """A simple chinese localizer"""
 
     def localize(self, msg):
-        """We'll punt if we don't have a translation"""
-        return self.translations.get(msg, msg)
+        return print('The message is translated to chinese.\n', msg)
 
 
 class EnglishLocalizer(object):
-    """Simply echoes the message"""
+    """A simple english localizer"""
 
     def localize(self, msg):
-        return msg
+        return print('The message is translated to english.\n', msg)
 
 
 def get_localizer(language="English"):
     """Factory"""
     localizers = {
         "English": EnglishLocalizer,
-        "Greek": GreekLocalizer,
+        "Chinese": ChineseLocalizer,
     }
     return localizers[language]()
 
 
 def main():
-    """
-    # Create our localizers
-    >>> e, g = get_localizer(language="English"), get_localizer(language="Greek")
+    elocalizer = get_localizer(language="English")
+    zlocalizer = get_localizer(language="Chinese")
 
-    # Localize some text
-    >>> for msg in "dog parrot cat bear".split():
-    ...     print(e.localize(msg), g.localize(msg))
-    dog σκύλος
-    parrot parrot
-    cat γάτα
-    bear bear
-    """
-
+    elocalizer.localize('Please localize to English.')
+    zlocalizer.localize('Please localize to Chinese.')
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    main()
